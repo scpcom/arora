8990bf9fc54963aae071560247fc714c6ea3a733
Sune Vuorela <sune@vuorela.dk>
2014-04-02 22:10:13 +0200

    Set defaultssl configuration to not include too weak ssl ciphers. Addresses Debian bug #739229. Fix inspired by Richard Moore's patch in https://codereview.qt-project.org/#patch,all,75943,5

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,24 @@
  */
 
 #include "browserapplication.h"
+#include <QSslConfiguration>
+#include <QSslCipher>
 
 #ifdef Q_OS_WIN
 #include "explorerstyle.h"
 #endif
+
+void setReasonableSslConfiguration() {
+    QSslConfiguration conf = QSslConfiguration::defaultConfiguration();
+    QList<QSslCipher> wantedCiphers;
+    Q_FOREACH(const QSslCipher& cipher, conf.ciphers()) {
+        if(cipher.usedBits()>=128) {
+	    wantedCiphers << cipher;
+	}
+    }
+    conf.setCiphers(wantedCiphers);
+    QSslConfiguration::setDefaultConfiguration(conf);
+}
 
 int main(int argc, char **argv)
 {
@@ -36,6 +50,7 @@ int main(int argc, char **argv)
 #ifdef Q_OS_WIN
     application.setStyle(new ExplorerStyle);
 #endif
+    setReasonableSslConfiguration();
     application.newMainWindow();
     return application.exec();
 }
